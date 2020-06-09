29adc9591beabfc6c0638226e047a2d88f76a2b0
ampaire <femayapr@gmail.com>
2020-06-09 16:36:12 -0600

    Add some styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
+
+const H4 = styled.h2`
+  font-weight: bold;
+  line-height: 52px;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          The intellectuals
-        </p>
+        <H4>The intellectuals</H4>
       </header>
     </div>
   );
