68d42ecc9f23dc443ca36e0ad941acb298b0bbd7
ampaire <femayapr@gmail.com>
2020-06-11 16:10:41 -0600

    Add Book to Booklist

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Book from '../components/Book';
 
-export default function BooksList() {
+const BooksList = ({ books }) => {
+  const { state } = books;
   return (
     <div>
       <table>
-        <tr>
-          <th>Book ID</th>
-          <th>Title</th>
-          <th>Category</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Book ID</th>
+            <th>Title</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.map(book => <Book key={book.id} book={book} />)}
+        </tbody>
       </table>
     </div>
   );
-}
+};
+BooksList.propTypes = {
+  books: PropTypes.instanceOf(Array).isRequired,
+};
+
+const mapStateToProps = state => ({ state });
+export default connect(mapStateToProps)(BooksList);
