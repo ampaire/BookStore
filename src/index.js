f9971888a6006f530be3d2af9ec2590b6d77ec9f
ampaire <femayapr@gmail.com>
2020-06-11 17:59:55 -0600

    Fix linting errorrs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import store from './reducers/index';
-import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
