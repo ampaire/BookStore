f1a408680331b4661e1f283c9908c4cceffc9be7
ampaire <femayapr@gmail.com>
2020-06-12 13:14:09 -0600

    Fix import errors

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Book from './Book';
+import Book from '../components/Book';
 
 const TABLE = styled.table`
 width: 600px
