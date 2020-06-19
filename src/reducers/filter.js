6e16f60b73d3a020e56a373c454cd2032dd83cb0
ampaire <femayapr@gmail.com>
2020-06-19 15:31:33 -0600

    Fix the category filter bug

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,9 +1,7 @@
-import { CHANGE_FILTER } from '../actions/index';
-
 const filterReducer = (state = 'All', action) => {
   switch (action.type) {
-    case CHANGE_FILTER:
-      return action.category;
+    case 'CHANGE_FILTER':
+      return action.book;
     default:
       return state;
   }
