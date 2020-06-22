7cf0e189949d14b27d5312e2ce2077ca7438d658
ampaire <femayapr@gmail.com>
2020-06-21 19:13:47 -0600

    working on UI

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,17 @@ import BooksForm from '../containers/BooksForm';
 function App() {
   return (
     <div className="App">
-      <BooksList />
-      <BooksForm />
+      <div className="container">
+        <header className="header">
+          <h1 className="title">The Intellectuals</h1>
+          <h4>BOOKS</h4>
+          <h4>CATEGORIES</h4>
+        </header>
+
+        <BooksList />
+        <BooksForm />
+      </div>
+
     </div>
   );
 }
